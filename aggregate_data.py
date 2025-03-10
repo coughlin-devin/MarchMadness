017567d4cdc6c242c9fe4d4209d8945916200512
Devin Coughlin <coughlindevin@gmail.com>
2025-03-10 19:37:41 -0400

    streamlined file, added features list for each data source

--- aggregate_data.py
+++ aggregate_data.py
@@ -2,52 +2,39 @@ import pandas as pd
 
 schedule = pd.read_csv(r"Data/Clean/clean_schedule.csv")
 ap = pd.read_csv(r"Data/Clean/clean_ap_poll.csv")
-
-player_pg_all = pd.read_csv(r"Data/Clean/clean_player_per_game_complete.csv")
 player_pg_pruned = pd.read_csv(r"Data/Clean/clean_player_per_game_pruned.csv")
-
-advanced = pd.read_csv(r"Data/Clean/clean_school_advanced.csv")
-basic_opp = pd.read_csv(r"Data/Clean/clean_basic_opponent.csv")
-advanced_opp = pd.read_csv(r"Data/Clean/clean_advanced_opponent.csv")
-
 team_opp = pd.read_csv(r"Data/Clean/clean_team_opp.csv")
+team_rankings = pd.read_csv(r"Data/Clean/team_rankings.csv")
+kenpom = pd.read_csv(r"Data/Clean/kenpom.csv")
+# TODO: incorporate this for single game model, need to attach team and opponnent features to df, maybe go back and include round of the matchups
+matchups = pd.read_csv(r"Data/Clean/clean_matchups.csv")
 
-# merge schedule and ap
-schedule_ap = schedule.merge(ap, how='left', on=['School', 'Year'])
+df = schedule.merge(ap, how='left', on=['School', 'Year'])
+df = df.merge(player_pg_pruned, how='left', on=['School', 'Year'])
+df = df.merge(team_opp, how='left', on=['School', 'Year'])
+df = df.loc[df['Year'] >= 1998]
+df = df.merge(team_rankings, how='left', on=['School', 'Year'])
+df = df.loc[df['Year'] >= 2002]
+df = df.merge(kenpom, how='left', on=['School', 'Year'])
 
-# TODO: revisit clean_player for added per_40 and per_100 data and to make sure imputation methods are concrete and can be done per year, add knn manhatten distance
-# merge player_pg_all and player_pg_pruned
-player = player_pg_all.merge(player_pg_pruned, how='left', on=['School', 'Year'], suffixes=['', '_Pruned'])
+# df.to_csv(r"Data/Clean/clean_aggregate.csv", mode='w', index=False)
 
-# merge basic_opp and advanced_opp
-school_opp = basic_opp.merge(advanced_opp, how='left', on=['School', 'Year'])
+# TODO: try excluding Pre, RSCI, and Seed with a head to head model
+features = ['School', 'Year', 'WINS']
+ap = ['Pre']
+schedule = ['SRS_OPP']
+player = ['Class_MW', 'Pos_MW', 'Height_MW', 'FG%_MW', 'Cohesion', 'Guard_RSCI Top 100_Mean', 'Forward_RSCI Top 100_Mean', 'Center_RSCI Top 100_Mean', 'Freshman_RSCI Top 100_Mean', 'Sophomore_RSCI Top 100_Mean', 'Junior_RSCI Top 100_Mean', 'Senior_RSCI Top 100_Mean']
+team_opp = ['Seed', 'PIR', 'CTR', 'CSC', 'CTC', 'SRS', 'Simple_POS']
+# TODO: TOV% uses 0.44 for free throw constant, calculate my own using 0.475
+team_rankings = ['average-scoring-margin', 'average-2nd-half-margin', 'floor-percentage', 'opponent-floor-percentage', 'win-pct-all-games', 'effective-possession-ratio', 'extra-chances-per-game', 'games-played', 'field-goals-made-per-game', 'assist--per--turnover-ratio', 'turnover-pct', 'assists-per-possession', 'assists-per-fgm', 'total-rebounding-percentage', 'offensive-rebounding-pct', 'block-pct', 'steal-pct', 'opponent-steal-pct', 'personal-foul-pct', 'percent-of-points-from-2-pointers', 'percent-of-points-from-3-pointers', 'percent-of-points-from-free-throws', 'opponent-percent-of-points-from-2-pointers', 'opponent-percent-of-points-from-3-pointers', 'opponent-percent-of-points-from-free-throws', 'effective-field-goal-pct', 'opponent-effective-field-goal-pct', 'three-point-pct', 'two-point-pct', 'free-throw-pct', 'true-shooting-percentage', 'opponent-true-shooting-percentage', 'fta-per-fga', 'opponent-fta-per-fga', 'points-from-2-pointers']
+kenpom = ['AdjEM', 'AdjOE', 'AdjDE', 'AdjTempo', 'Pyth']
 
-# merge advanced stats with schedule and ap
-advanced_schedule_ap = advanced.merge(schedule_ap, how='left', on=['School', 'Year'])
+features.extend(ap)
+features.extend(schedule)
+features.extend(player)
+features.extend(team_opp)
+features.extend(team_rankings)
+features.extend(kenpom)
 
-# merge player stats with advanced stats and schedule and ap
-advanced_schedule_ap_player = advanced_schedule_ap.merge(player, how='left', on=['School', 'Year'])
-
-df = team_opp.merge(advanced_schedule_ap_player, how='left', on=['School', 'Year'])
-
-# NOTE: Four Factors: eFG% (40%), TOV% (25%), ORB% & DRB% (20%), FT/FGA (15%)
-# eFG%
-df['EFG%'] = round((df['2P'] + 1.5*df['3P']) / df['FGA'], 3)
-df['EFG%_OPP'] = round((df['2P_OPP'] + 1.5*df['3P_OPP']) / df['FGA_OPP'], 3)
-
-# TOV%
-df['TOV%']
-df['TOV%_OPP'] = round(df['TOV_OPP'] / (df['FGA_OPP'] + (0.475*df['FTA_OPP']) + df['TOV_OPP']), 3)
-
-# ORB% & DRB%
-df['ORB%']
-df['DRB%'] = round(df['DRB'] / (df['ORB_OPP'] + df['DRB']), 3)
-
-# FTA / FGA
-df['FTr']
-df['FTr_OPP'] = round(df['FTA_OPP'] / df['FGA_OPP'])
-
-# TODO: weight these features to use in the model? Or will the model decide on appropriate weights itself?
-four_factors = df.loc[:, ['School', 'Year', 'EFG%', 'EFG%_OPP', 'TOV%', 'TOV%_OPP', 'ORB%', 'DRB%', 'FTr', 'FTr_OPP']]
-
-df.to_csv(r"Data/Clean/clean_aggregate.csv", mode='w', index=False)
+features = df[features]
+# features.to_csv(r"Data/Clean/features.csv", mode='w', index=False)
